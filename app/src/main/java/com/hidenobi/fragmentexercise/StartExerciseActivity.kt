0940734bff26b10cbe6512565baa157a71776f7b
LordierClaw <lordierclaw@gmail.com>
2023-07-23 18:09:51 +0700

    Design start_exercise

--- app/src/main/java/com/hidenobi/fragmentexercise/StartExerciseActivity.kt
+++ app/src/main/java/com/hidenobi/fragmentexercise/StartExerciseActivity.kt
@@ -3,9 +3,9 @@ package com.hidenobi.fragmentexercise
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StartExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start_exercise)
     }
 }
