de56865272ac82a6e6fed5c4ec8faa77d59913b4
Wos2610 <nt2nguyenthingocthuy@gmail.com>
2023-07-23 22:19:03 +0700

    Commit

---
+++ app/src/main/java/com/hidenobi/fragmentexercise/StartExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.hidenobi.fragmentexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StartExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_exercise)
+    }
+}
