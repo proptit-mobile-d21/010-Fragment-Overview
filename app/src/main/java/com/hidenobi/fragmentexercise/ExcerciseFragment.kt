d77e9f96868f8646c02857e335270b122d51c645
Zero-zx <dthieu2512@gmail.com>
2023-07-25 00:47:29 +0700

    testing fragment

---
+++ app/src/main/java/com/hidenobi/fragmentexercise/ExcerciseFragment.kt
@@ -0,0 +1,12 @@
+package com.hidenobi.fragmentexercise
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ExcerciseFragment : Fragment(R.layout.fragment_excercise) {
+
+}
