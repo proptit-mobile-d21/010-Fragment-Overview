d2bcf776014235bbbe131c6b6dad7180ef695a21
Wos2610 <nt2nguyenthingocthuy@gmail.com>
2023-07-28 18:43:11 +0700

    Circle Progress

--- app/src/main/java/com/hidenobi/fragmentexercise/ExerciseRunFragment.kt
+++ app/src/main/java/com/hidenobi/fragmentexercise/ExerciseRunFragment.kt
@@ -1,12 +1,18 @@
 package com.hidenobi.fragmentexercise
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.CountDownTimer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResultListener
 import com.hidenobi.fragmentexercise.databinding.FragmentExerciseRunBinding
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,8 +42,7 @@ class ExerciseRunFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_exercise_run, container, false)
@@ -47,9 +52,11 @@ class ExerciseRunFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentExerciseRunBinding.bind(view)
 
-        setFragmentResultListener("time_left"){requestKey, bundle ->
+        setFragmentResultListener("time_left") { requestKey, bundle ->
             min = bundle.getInt("hour") * 60 + bundle.getInt("min")
             binding.timeLeft.text = String.format("%02d:%02d", min, sec)
+            binding.circleProgress.progress = 100
+            startTimer(min)
         }
     }
 
@@ -64,12 +71,32 @@ class ExerciseRunFragment : Fragment() {
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ExerciseRunFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        fun newInstance(param1: String, param2: String) = ExerciseRunFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_PARAM1, param1)
+                putString(ARG_PARAM2, param2)
             }
+        }
+    }
+
+    private fun startTimer(minutes : Int){
+        val secondSum = min * 60
+        object : CountDownTimer((secondSum).toLong() * 1000, 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                val minutesRemaining = millisUntilFinished / 1000 / 60
+                val secondsRemaining = (millisUntilFinished / 1000) % 60
+                binding.apply {
+                    binding.timeLeft.text =
+                        String.format("%02d:%02d", minutesRemaining, secondsRemaining)
+                    binding.circleProgress.progress = ((millisUntilFinished.toFloat()/(secondSum.toLong() * 1000).toFloat()) * 100).toInt()
+                }
+
+            }
+
+            override fun onFinish() {
+                Toast.makeText(context, "Timer end", Toast.LENGTH_LONG).show()
+            }
+        }.start()
     }
 }
